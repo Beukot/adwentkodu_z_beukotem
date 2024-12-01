4b49c4ecfec31a139a12264439cff37973b761d0
Beukot <michalek7790@gmail.com>
2024-12-01 19:34:08 +0100

    day one part two solved

--- dayOne/dayOne.hpp
+++ dayOne/dayOne.hpp
@@ -28,22 +28,38 @@ inline void readFile(std::vector<int>* leftList, std::vector<int>* rightList) {
 }
 
 template <typename T>
-void bubbleSort(T& arr, std::size_t len)
-{
-    for (size_t i = 0; i < len; i++)
-    {
-        for (size_t j = 0; j < len; j++)
-        {
-            if (arr[i] < arr[j])
-            {
+void bubbleSort(T& arr, std::size_t len) {
+    for (size_t i = 0; i < len; i++) {
+        for (size_t j = 0; j < len; j++) {
+            if (arr[i] < arr[j]) {
                 std::swap(arr[i], arr[j]);
             }
         }
     }
 }
 
+template <typename T>
+int similarityScore(T& leftList, T& rightList, std::size_t len) {
+    int score = 0;
+
+    for (size_t i = 0; i < len; i++) {
+        int timesAppeared = 0;
+
+        for (size_t j = 0; j < len; j++) {
+            if (leftList[i] == rightList[j]) {
+                timesAppeared++;
+            }
+        }
+
+        score += timesAppeared * leftList[i];
+    }
+
+    return score;
+}
+
 inline void dayOneMain() {
     int sum = 0;
+    int score = 0;
     std::vector<int> leftList, rightList;
 
     readFile(&leftList, &rightList);
@@ -51,13 +67,14 @@ inline void dayOneMain() {
     bubbleSort(leftList, leftList.size());
     bubbleSort(rightList, rightList.size());
 
-
-
     for (int i = 0; i < leftList.size(); i++) {
         sum += abs(leftList[i] - rightList[i]);
     }
 
-    std::cout << "Rozwiązaniem zadania 1 jest: " << sum << "." << std::endl;
+    score = similarityScore(leftList, rightList, leftList.size());
+
+    std::cout << "Rozwiązaniem części pierwszej zadania 1 jest: " << sum << "." << std::endl;
+    std::cout << "Rozwiązaniem części drugiej zadania 1 jest: " << score << "." << std::endl;
 }
 
 #endif //DAYONE_HPP
