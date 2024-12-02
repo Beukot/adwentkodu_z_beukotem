4c4fa3bb10498a2c73eb3f6a77e916a671d632e6
Beukot <michalek7790@gmail.com>
2024-12-02 18:05:05 +0100

    day two part two solved

--- dayTwo/dayTwo.hpp
+++ dayTwo/dayTwo.hpp
@@ -39,13 +39,14 @@ inline bool determineSafety(std::vector<int> arr) {
     return true;
 }
 
-inline int readFile() {
+inline void readFile(int* res, int* resD) {
     std::ifstream file("../dayTwo/input.txt");
     std::string word;
     std::string line;
     std::vector<int> levels;
 
     int safeReports = 0;
+    int safeReportsWithDampener = 0;
 
     while (std::getline(file, line)) {
         for (int i = 0; i < line.size(); i++) {
@@ -63,21 +64,40 @@ inline int readFile() {
 
         if (determineSafety(levels)) {
             safeReports++;
+            safeReportsWithDampener++;
+        } else {
+            for (int i = 0; i < levels.size(); i++) {
+                std::vector<int> levelsWithDampener = levels;
+                levelsWithDampener.erase(levelsWithDampener.begin() + i);
+
+                if (determineSafety(levelsWithDampener)) {
+                    safeReportsWithDampener++;
+                    break;
+                }
+            }
         }
+
+
 
         levels.clear();
     }
 
     file.close();
 
-    return safeReports;
+    *res = safeReports;
+    *resD = safeReportsWithDampener;
 }
 
 inline void dayTwoMain() {
-    int res = readFile();
+    int res;
+    int resD;
+
+    readFile(&res, &resD);
 
     std::cout << "Część pierwsza zadania 2:" << std::endl;
     std::cout << "Bezpiecznych raportów: " << res << std::endl;
+    std::cout << "Część druga zadania 2:" << std::endl;
+    std::cout << "Bezpiecznych raportów: " << resD << std::endl;
 }
 
 #endif //DAYTWO_HPP
