963ed29fece07207a5d92d92e1e9d40f022600d6
Beukot <michalek7790@gmail.com>
2024-12-05 14:47:32 +0100

    day four recursive word search (not working)

--- dayFour/dayFour.hpp
+++ dayFour/dayFour.hpp
@@ -18,30 +18,47 @@ inline void readFile(std::vector<std::string>& arr) {
     file.close();
 }
 
+inline bool checkBounds(std::vector<std::string>& arr, int X, int Y) {
+    if (X >= 0 && Y >= 0 && X < arr.size() && Y < arr[0].size()) {
+        return true;
+    }
+
+    return false;
+}
+
+inline bool checkCharRecursive(std::vector<std::string>& arr, std::string word, int startX, int startY, int dirX, int dirY, int index) {
+    if (!checkBounds(arr, startX + dirX, startY + dirY))  {
+        return false;
+    }
+
+    if (index == word.size()) {
+        return true;
+    }
+
+    if (word[index] == arr[startX + dirX][startY + dirY]) {
+        startX += dirX;
+        startY += dirY;
+        index += 1;
+
+        return checkCharRecursive(arr, word, startX, startY, dirX, dirY, index);
+    }
+
+    return false;
+}
+
 inline int searchForWord(std::vector<std::string>& arr) {
     int occured = 0;
 
     for (int i = 0; i < arr.size(); i++) {
         for (int j = 0; j < arr[i].size(); j++) {
             if (arr[i][j] == 'X') {
-                if (arr[i][j + 1] == 'M') {
-                    if (arr[i][j + 2] == 'A' && arr[i][j + 3] == 'S') {
-                        occured++;
-                    }
-                }
-                if (arr[i][j - 1] == 'M') {
-                    if (arr[i][j - 2] == 'A' && arr[i][j - 3] == 'S') {
-                        occured++;
-                    }
-                }
-                if (arr[i + 1][j] == 'M' && i + 3 < arr.size()) {
-                    if (arr[i + 2][j] == 'A' && arr[i + 3][j] == 'S') {
-                        occured++;
-                    }
-                }
-                if (arr[i - 1][j] == 'M' && i - 3 >= 0) {
-                    if (arr[i - 2][j] == 'A' && arr[i - 3][j] == 'S') {
-                        occured++;
+                for (int k = -1; k <= 1; k++) {
+                    for (int l = -1; l <= 1; l++) {
+                        if (l != 0 && k != 0) {
+                            if (checkCharRecursive(arr, "MAS", i, j, k, l, 0)) {
+                                occured++;
+                            }
+                        }
                     }
                 }
             }
@@ -54,6 +71,7 @@ inline int searchForWord(std::vector<std::string>& arr) {
 inline void dayFourMain() {
     std::vector<std::string> lines;
     readFile(lines);
+    std::string word = "XMAS";
 
     std::cout << searchForWord(lines) << std::endl;
 }
